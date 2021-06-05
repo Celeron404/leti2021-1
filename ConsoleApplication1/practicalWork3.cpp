b66ed9e96f3246406358623b51ab754c962edb40
Sergey <52708090+Celeron404@users.noreply.github.com>
2021-06-05 17:23:21 +0300

    add: isExpression with regex

--- ConsoleApplication1/practicalWork3.cpp
+++ ConsoleApplication1/practicalWork3.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <regex>
 using namespace std;
 
 
@@ -6,12 +8,12 @@ void practicalWork3() {
 	system("CLS");
 	cout << "Solution of task \"Working with stacks and queues, learning direct and reverse polish notation.\n";
 	bool inputIsCorrect = false;
+	short exprType;
 	do {
 		cout << "\nEnter the type of expression:\n"
 			<< "1) Simple notation\n"
 			<< "2) Direct polish notation\n"
 			<< "3) Reverse polish notation\n>> ";
-		int exprType;
 		cin >> exprType;
 		if (exprType == 1 || exprType == 2 || exprType == 3) {
 			inputIsCorrect = true;
@@ -19,5 +21,50 @@ void practicalWork3() {
 		}
 		cout << "Error: wrong input. Please repeat menu selection.\n";
 	} while (!inputIsCorrect);
+
+	// enter and checking
+	inputIsCorrect = false;
+	do {
+		cout << "\nEnter the expression. Example: ";
+		switch (exprType) {
+		case 1:
+			cout << "(x + 3) * y";
+			break;
+		case 2:
+			cout << "* + x 3 y";
+			break;
+		case 3:
+			cout << "x 3 + y *";
+		}
+		cout << "\n>> ";
+		string input;
+		cin >> input;
+
+		if (isExpression(input))
+			toExpression(&input);
+		else {
+			cout << "Input contains wrong symbols. \n" <<
+				"Correct symbols: + - * / ( ) \"space\" 0-9 a-z A-Z \n" <<
+				"Please, repeat input\n";
+			continue;
+		}
+
+		bool checkExpr = expressionIsCorrect();
+	} while (!inputIsCorrect);
 	system("pause");
+}
+
+bool isExpression(string input) {
+	regex mask("[^A-Za-z0-9\s\+\-\*\/\(\)]");
+	if (std::regex_search(input, mask))
+		return false;
+	true;
+}
+
+void toExpression(string * input) {
+
+}
+
+bool expressionIsCorrect(string input, short exprType) {
+
 }
