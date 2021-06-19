f81f8ded4332e019b4354559abc561e956778611
Sergey <52708090+Celeron404@users.noreply.github.com>
2021-06-19 14:20:58 +0300

    rework: working only with numbers, no variables

--- ConsoleApplication1/practicalWork3.cpp
+++ ConsoleApplication1/practicalWork3.cpp
@@ -33,13 +33,13 @@ void practicalWork3() {
 		cout << "\nEnter the expression. Example: ";
 		switch (exprType) {
 		case 1:
-			cout << "(x + 3) * y";
+			cout << "(1 + 3) * 4";
 			break;
 		case 2:
-			cout << "* + x 3 y";
+			cout << "* + 1 3 4";
 			break;
 		case 3:
-			cout << "x 3 + y *";
+			cout << "1 3 + 4 *";
 		}
 		cout << "\n>> ";
 		string input;
@@ -60,7 +60,7 @@ void practicalWork3() {
 
 		/*else {
 			cout << "input contains wrong symbols. \n" <<
-				"correct symbols: + - * / ( ) \"space\" 0-9 a-z a-z \n" <<
+				"correct symbols: + - * / ( ) \"space\" 0-9 \n" <<
 				"please, repeat input\n";
 			continue;
 		}*/
@@ -84,69 +84,69 @@ void removeSpacesFromString(string &input) {
 
 void expressionPrimaryCheck(string input) {
 	//searching incorrect symbols
-	regex mask("[^A-Za-z0-9\\s\\+\\-\\*\\/\\(\\)]");
+	regex mask("[^0-9\\s\\+\\-\\*\\/\\(\\)]");
 	if (std::regex_search(input, mask))
 		/*
 		Error! Input contains wrong symbols.
-		Correct symbols: + - * / ( ) \"space\" 0-9 a-z a-z
+		Correct symbols: + - * / ( ) \"space\" 0-9
 		Please, repeat input
 		*/
-		throw "Error! Input contains wrong symbols. \nCorrect symbols: + - * / ( ) \"space\" 0-9 a-z a-z \nPlease, repeat input.\n";
+		throw "Error! Input contains wrong symbols. \nCorrect symbols: + - * / ( ) \"space\" 0-9 \nPlease, repeat input.\n";
 
 	//searching incorrect sequence of characters
-	mask = "[A-Za-z0-9]\\s[A-Za-z0-9]"; // "11 4 x y" sequences
+	mask = "[0-9]\\s[0-9]"; // "11 4 x y" sequences
 	if (std::regex_search(input, mask))
 		/*
 		Error! Input contains wrong sequence of characters.
-		Correct example: (x * y + 3 * 4) * y
-		Incorrect sequences example: (x y + 3 4) * Y
+		Correct example: (1 * 4 + 3 * 4) * 4
+		Incorrect sequences example: (1 4 + 3 4) * 4
 		Please, repeat input.
 		*/
-		throw "Error! Input contains wrong sequence of characters. \nCorrect example: (x + 3) * y\nIncorrect sequences example: +--*/++--**// xx yy \nPlease, repeat input.\n";
+		throw "Error! Input contains wrong sequence of characters. \nCorrect example: (1 + 3) * 4\nIncorrect sequences example: +--*/++--**// \nPlease, repeat input.\n";
 }
 
 void expressionSecondaryCheck(string input) {
 	//searching incorrect sequence of characters
-	regex mask("[A-Za-z\\+\\-\\*\\/]{2}"); // "+--*/++--**// xx yy" sequences
+	regex mask("[\\+\\-\\*\\/]{2}"); // "+--*/++--**//" sequences
 	if (std::regex_search(input, mask))
 		/*
 		Error! Input contains wrong sequence of characters.
-		Correct example: (x + 3) * y
-		Incorrect sequences example: +--* /++--** // xx yy
+		Correct example: (1 + 3) * 4
+		Incorrect sequences example: +--* /++--** //
 		Please, repeat input.
 		*/
-		throw "Error! Input contains wrong sequence of characters. \nCorrect example: (x + 3) * y\nIncorrect sequences example: +--*/++--**// xx yy \nPlease, repeat input.\n";
+		throw "Error! Input contains wrong sequence of characters. \nCorrect example: (x + 3) * y\nIncorrect sequences example: +--*/++--**// \nPlease, repeat input.\n";
 
-	mask = "([A-Za-z0-9]\\()|(\\)[A-Za-z0-9])|(\\(\\))"; // "() 5( )5 x( )x Y( )Y" sequences
+	mask = "([0-9]\\()|(\\)[0-9])|(\\(\\))"; // "() 5( )5" sequences
 	if (std::regex_search(input, mask))
 		/*
 		Error! Input contains wrong sequence of characters.
-		Correct example: (x + 3) * y
-		Incorrect sequences example: () 5( )5 x( )x Y( )Y
+		Correct example: (1 + 3) * 4
+		Incorrect sequences example: () 5( )5
 		Please, repeat input.
 		*/
-		throw "Error! Input contains wrong sequence of characters. \nCorrect example: (x + 3) * y\nIncorrect sequences example: () 5( )5 x( )x Y( )Y \nPlease, repeat input.\n";
+		throw "Error! Input contains wrong sequence of characters. \nCorrect example: (1 + 3) * 4\nIncorrect sequences example: () 5( )5 \nPlease, repeat input.\n";
 	
 	//searching the operators in begin and end of the expression
 	mask = "^[\\+\\*\\/]|[\\+\\-\\*\\/]$";
 	if (std::regex_search(input, mask))
 		/*
 		Error! Input contains operators in begin or end of the expression.
-		Correct example: x + 3 * y
-		Incorrect example: *x + 3 * y-
+		Correct example: 1 + 3 * 4
+		Incorrect example: *1 + 3 * 4-
 		Please, repeat input.
 		*/
-		throw "Error! Input contains operators in begin or end of the expression. \nCorrect example: x + 3 * y\nIncorrect example: *x + 3 * Y- \nPlease, repeat input.\n";
+		throw "Error! Input contains operators in begin or end of the expression. \nCorrect example: 1 + 3 * 4\nIncorrect example: *1 + 3 * 4- \nPlease, repeat input.\n";
 
 	//counting opened and closed brackets
 	if (!bracketCheck(input))
 		/*
 		Error! The number of opening brackets does not match the number of closing brackets.
-		Correct example: (x + (3 - 1)) * y
-		Incorrect example: (x + (3 - q) * y
+		Correct example: (1 + (3 - 1)) * 4
+		Incorrect example: (1 + (3 - 4) * 4
 		Please, repeat input.
 		*/
-		throw "Error! The number of opening brackets does not match the number of closing brackets. \nCorrect example: (x + (3 - 1)) * y\nIncorrect example: (x + (3 - q) * Y \nPlease, repeat input.\n";
+		throw "Error! The number of opening brackets does not match the number of closing brackets. \nCorrect example: (1 + (3 - 1)) * 4\nIncorrect example: (1 + (3 - q) * 4 \nPlease, repeat input.\n";
 }
 
 bool bracketCheck(string input) {
